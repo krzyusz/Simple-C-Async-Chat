3651df35987c62658bd1d8c80fb01fbf9f6a9864
krzyusz <69759720+krzyusz@users.noreply.github.com>
2021-02-14 12:08:49 +0100

    Delete czat_server.c

--- czat_server.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/socket.h> //For Sockets
-#include <stdlib.h>
-#include <netinet/in.h> //For the AF_INET (Address Family)
-
-struct sockaddr_in serv; //This is our main socket variable.
-int fd; //This is the socket file descriptor that will be used to identify the socket
-int conn; //This is the connection file descriptor that will be used to distinguish client connections.
-char message[100] = ""; //This array will store the messages that are sent by the server
